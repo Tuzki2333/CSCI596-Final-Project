1601d35761fc3d617e0dd5c7fbbbe2f9538093a4
Tuzki2333 <66580690+Tuzki2333@users.noreply.github.com>
2023-12-08 18:04:23 +0800

    Add inputs & modify original code

--- knapsack_lagrangian.c
+++ knapsack_lagrangian.c
@@ -1,56 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define MAX_ITER 100
 #define EPSILON 0.0001
-#define LEARNING_RATE 0.01
+#define INITIAL_ALPHA 0.0001
 
 int main(int argc, char *argv[]) {
 
     FILE *fp;
-    unsigned long n, w;
+    unsigned long n, w; /* n denotes the number of items and w denotes the space limit */
 
-    fp = fopen("inputs/knapsackInstance-n-10-W-1.txt", "r");
+    fp = fopen("inputs/knapsackInstance-n-100000-W-10000-seed-5.txt", "r");
     fscanf(fp, "%lu", &n);
     fscanf(fp, "%lu", &w);
 
-    printf("%lu %lu\n", n, w);
+    printf("The number of items: %lu\n", n);
+    printf("The space limit: %lu\n", w);
 
-    double *v_list;
-    double *w_list;
+    double *v_list; /* the values of items */
+    double *w_list; /* the weights of items */
 
     v_list = (double*)malloc(n*sizeof(double));
     w_list = (double*)malloc(n*sizeof(double));
 
-    for (int i=0; i<n; i++){
+    for (unsigned long i=0; i<n; i++){
         fscanf(fp, "%lf", &v_list[i]);
         fscanf(fp, "%lf", &w_list[i]);
     }
 
-    for (int i=0; i<n; i++){
+    /*
+    for (unsigned long i=0; i<n; i++){
         printf("%lf %lf\n", v_list[i], w_list[i]);
     }
+    */
 
     fclose(fp);
 
-    double lambda = 0;
+    double cpu1 = ((double) clock())/CLOCKS_PER_SEC;
 
-    for (int t=1; t<=MAX_ITER; t++){
+    /* initialize lambda as 0 */
+    double lambda = 0;
+    double last_w = 0;
+    double alpha = INITIAL_ALPHA;
+
+    for (unsigned long t=1; t<=MAX_ITER; t++){
+
         double current_w = 0;
         double current_v = 0;
-        for (int i=0; i<n; i++){
+        for (unsigned long i=0; i<n; i++){
             if (v_list[i] - lambda*w_list[i] > 0){
                 current_w += w_list[i];
                 current_v += v_list[i];
             }
         }
-        printf("%d %lf %lf %lf \n", t, lambda, current_w, current_v);
-        if ((current_w <= (1+EPSILON)*w)&&(current_w >= (1-EPSILON)*w)){
+
+        printf("%d %lf %lf %lf %lf \n", t, lambda, alpha, current_w, current_v);
+        if (abs(current_w-w) <= EPSILON*w){
             break;
         }
-        lambda += LEARNING_RATE * (current_w - w);
+        
+        lambda += alpha * (current_w - w);
         if (lambda < 0){
             lambda = 0;
         }
+
+        if ((t>1)&&(abs(last_w-w)<abs(current_w-w))){
+            alpha *= 0.5;
+        }
+        last_w = current_w;
     }
+
+    /* obtain final results */
+
+    double final_w = 0;
+    double final_v = 0;
+
+    for (int i=0; i<n; i++){
+        if ((v_list[i] - lambda*w_list[i] > 0)&&(final_w + w_list[i] <= w)){
+            final_w += w_list[i];
+            final_v += v_list[i];
+        }
+    }
+
+    double cpu2 = ((double) clock())/CLOCKS_PER_SEC;
+
+    double cpu = cpu2 - cpu1;
+
+    printf("Final space used: %lf\n", final_w);
+    printf("Total values obtained: %lf\n", final_v);
+    printf("Execution time (s): %lf\n", cpu);
+
 }
